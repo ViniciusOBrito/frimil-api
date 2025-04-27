29ed4700e781b9b72de0ea935e859cfb7cdf4890
vinicius <peixonauta.vb@gmail.com>
2025-04-27 15:28:55 -0300

    feat: configurando perfis de acesso para os endpoints

--- src/main/java/com/frimil/frimilapi/seguranca/SegurancaConfig.java
+++ src/main/java/com/frimil/frimilapi/seguranca/SegurancaConfig.java
@@ -3,10 +3,12 @@ package com.frimil.frimilapi.seguranca;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,11 +25,18 @@ public class SegurancaConfig {
 
 
     private final String[] ENDPOINTS_RELEASED = {
-
+            "autenticacao/login",
+            "autenticacao/register",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
     };
 
     private final String[] ENDPOINTS_ADMIN = {
-
+            "fazenda/**",
+            "pecuarista/**",
+            "pesador-gado/**",
+            "transportadores/**",
+            "veiculo/**"
     };
 
     private final String[] ENDPOINTS_ADMIN_USER = {
@@ -38,10 +47,16 @@ public class SegurancaConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 
         httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers( ENDPOINTS_RELEASED).permitAll()
+
+                        .requestMatchers(HttpMethod.GET, ENDPOINTS_ADMIN).hasAnyAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, ENDPOINTS_ADMIN).hasAnyAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, ENDPOINTS_ADMIN).hasAnyAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, ENDPOINTS_ADMIN).hasAnyAuthority("ADMIN")
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(filtroSeguranca, UsernamePasswordAuthenticationFilter.class);
